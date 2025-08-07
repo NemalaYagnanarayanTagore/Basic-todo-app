5e931c5decc255c21c3795bbbfb5d4ea8009a146
NemalaYagnanarayanTagore <nemalatagore112006@gmail.com>
2025-08-07 08:26:40 +0530

    updated index by adding search functionality, menu and profile.

--- app/index.tsx
+++ app/index.tsx
@@ -1,24 +1,90 @@
-import { FlatList, Text, View } from "react-native";
+import { Ionicons } from "@expo/vector-icons";
+import { FlatList, Image, StyleSheet, Text, TextInput, TouchableOpacity, View } from "react-native";
+import { SafeAreaView } from "react-native-safe-area-context";
 
 export default function Index() {
 
   const todoData = [
-
-  ]
+    {
+      id : 1,
+      title : "Todo 1",
+      isDone : false,
+    },
+    {
+      id : 2,
+      title : "Todo 2",
+      isDone : false,
+    },{
+      id : 3,
+      title : "Todo 3",
+      isDone : false,
+    },{
+      id : 4,
+      title : "Todo 4",
+      isDone : false,
+    },{
+      id : 5,
+      title : "Todo 5",
+      isDone : false,
+    },{
+      id : 6,
+      title : "Todo 6",
+      isDone : false,
+    },
+  ];
 
 
 
 
   return (
-    <View
-      style={{
-        flex: 1,
-        justifyContent: "center",
-        
-        alignItems: "center",
-      }}
+    <SafeAreaView
+      style={styles.container}
     >
-      <FlatList data={todoData} keyExtractor={(item) =>} />
-    </View>
+      <View style = {styles.header}>
+        <TouchableOpacity onPress={() =>{alert('clicked clicked menu!')}}>
+          <Ionicons name="menu" size={24} color={'#333'} />
+        </TouchableOpacity>
+        <TouchableOpacity onPress={() => {alert('you clicked profile!')}}>
+          <Image source={{ uri: 'https://xsgames.co/randomusers/avatar.php?g=male'}} style = {{width:40, height: 40, borderRadius:20}} />
+        </TouchableOpacity>
+      </View>
+      <View style = {styles.searchBar}>
+        <Ionicons name="search" size={24} color={'#333'} />
+        <TextInput placeholder="Search" style={styles.searhInput} />
+      </View>
+      <FlatList data={todoData} keyExtractor={(item) => item.id.toString()} renderItem={({item}) => (
+        <View>
+          <Text>{item.title}</Text>
+        </View>
+      )} />
+    </SafeAreaView>
   );
 }
+
+const styles = StyleSheet.create({
+  container :{
+    flex: 1,
+    paddingHorizontal: 20,
+    backgroundColor: '#f5f5f5',
+  },
+  header:{
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    alignItems: 'center',
+    marginBottom: 20,
+  },
+  searchBar:{
+    flexDirection: 'row',
+    backgroundColor: '#fff',
+    padding: 16,
+    borderRadius: 10,
+    gap: 10,
+    alignItems: 'center',
+    marginBottom:20,
+  },
+  searhInput:{
+    flex:1, 
+    fontSize:16,
+    color: '#333',
+  }
+});
